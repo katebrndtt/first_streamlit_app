4202847f22d6c402dff606a65f65fcb173495b2d
katebrndtt <144973485+katebrndtt@users.noreply.github.com>
2023-09-14 14:43:08 +0800

    Update streamlit_app.py
    added subtitle 2
    import requests
    adds the api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit
-
+#Title
 streamlit.title('My Parents New Healthy Diner')
 
+#Subtitle 1
 streamlit.header('Breakfast Favorites')
 
+#Menu Options
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+#Subtitle 2
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
@@ -22,3 +25,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
