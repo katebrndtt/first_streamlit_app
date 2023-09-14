febc74b192dd1f838a6a5333e7f41c3771468878
katebrndtt <144973485+katebrndtt@users.noreply.github.com>
2023-09-14 10:33:33 +0800

    Update streamlit_app.py
    imported pandas
    read csv file
    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
-import streamlit as st
+import streamlit
 
-st.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
-st.header('Breakfast Favorites')
+streamlit.header('Breakfast Favorites')
 
-st.text('🥣 Omega 3 & Blueberry Oatmeal')
-st.text('🥗 Kale, Spinach & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
